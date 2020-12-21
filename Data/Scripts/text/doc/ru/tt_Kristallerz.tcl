ef5e0c4eb5e21f8e561e426cb4968264231b4c17
Christian Voß <cech12@gmail.com>
2020-12-21 13:13:28 +0100

    add Russian language support

---
+++ Data/Scripts/text/doc/ru/tt_Kristallerz.tcl
@@ -0,0 +1,15 @@
+textwin clear
+call /scripts/misc/onlinehelputils.tcl
+ohlp_initstyle
+textwin print "/(is10)/(ildata/gui/icons/Kristallerz.tga)"
+
+// ---- text starts here - do not change anything above this line ---
+
+ohlp_ttheadlinestyle
+textwin print "Кристальная руда"
+ohlp_tttextbodystyle
+textwin print "With a massive hammer and the Hand of Odin you can beat coal to exquisite Crystal Ore."
+
+// ---- do not change anything below this line ---
+
+textwin print "/p"
